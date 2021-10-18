63b0986ea8ca73b288fe1e15caf70fab68c367f8
Fedor Glazov <glazov@math.tu-berlin.de>
2021-10-18 16:14:21 +0200

    Fix classifier.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: Creative Commons Zero v1.0 Universal",
+        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
         "Operating System :: OS Independent",
     ],
 )

f089a984e7273ffb8a88808e4e0c4df68ec525c1
Fedor Glazov <fedorglazov@gmail.com>
2021-12-28 15:10:33 +0100

    Specify where to look for packages.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(where="src"),
     package_dir={"": "src"},
     classifiers=[
         "Programming Language :: Python :: 3",
